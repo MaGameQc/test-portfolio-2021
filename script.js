a8fed4d50e6b6de04b33c6135c178cf2d1aec84f
TommyAudet <tommyaudet@magame.ca>
2020-09-16 16:50:13 -0400

    affichage des descriptions corrigé

--- script.js
+++ script.js
@@ -4,18 +4,13 @@ setTimeout(function () {
 
   // $("#doorBackground").css("display", "none");
   $("#doorContainer").css("display", "none");
-
 }, 2500);
 
 $("#aboutMe").click(function () {
+  $("#aboutMeText").empty();
 
-
-
-
-$("#aboutMeText").empty();
-
-
-  var text = 'Bonjour, moi cest Tommy, programmeur web front-end qui compte bien devenir full stack un jour. Je suis animateur de podcast, compositeur et ce qui me passionne par dessu tout, cest la technologie en générale. Un jour alors que je travaillais tout les jours sur une application web, je me suis posé la question "mais comment ont ils créer ce site web", et depuis, jai jamais cesser de programmer';
+  var text =
+      'Bonjour, moi cest Tommy, programmeur web front-end qui compte bien devenir full stack un jour. Je suis animateur de podcast, compositeur et ce qui me passionne par dessu tout, cest la technologie en générale. Un jour alors que je travaillais tout les jours sur une application web, je me suis posé la question "mais comment ont ils créer ce site web", et depuis, jai jamais cesser de programmer';
   var writeTextIn = document.getElementById("aboutMeText");
   var i = 0;
 
@@ -79,7 +74,7 @@ $("#aboutMe").click(function () {
   $("#contactMeMainContainer").css("display", "none");
 });
 
-$("#contactMe").click(function(){
+$("#contactMe").click(function () {
   $("#aboutMeContainer").css("display", "none");
   $("#projectCont").css("display", "none");
   $("#contactMeMainContainer").css("display", "grid");
@@ -166,7 +161,6 @@ animateHamMenuStepTwo = () => {
   );
 };
 
-
 // $(".projectsTestButton").on("mouseover", function(){
 //     let getElementIndex = $(this).index();
 //     alert(getElementIndex);
@@ -178,62 +172,92 @@ animateHamMenuStepTwo = () => {
 // });
 
 var autoTyper = {
-  description1 : "Ceci est un projet que j'ai réaliser afin d'apprendre le piano. apprendre les accords, les gammes majeurs et mineurs et surtout, me servir du WEB MIDI API afin qu'il soit compatible avec les clavier MIDI brancher par usb. Ce projet m'as appris qu'il y as étonnament une logique mathématique derrirrière la musique et que c'est donc facilement programmable.",
-  description2 : "ceci est un projet que j'ai eu en tête pendant très longtemps",
-  description3 : "ceci est un projet que j'ai eu en tête pendant très longtemps",
+  description1:
+      "Ceci est un projet que j'ai réaliser afin d'apprendre le piano. apprendre les accords, les gammes majeurs et mineurs et surtout, me servir du WEB MIDI API afin qu'il soit compatible avec les clavier MIDI brancher par usb. Ce projet m'as appris qu'il y as étonnament une logique mathématique derrirrière la musique et que c'est donc facilement programmable.",
+  description2: "ceci est le test no 2 ceci est le test no 2 ceci est le test no 2 ceci est le test no 2 ceci est le test no 2 ceci est le test no 2 ceci est le test no 2",
+  description3: "test no 3 test no 3 test no 3 test no 3 test no 3 test no 3 test no 3",
 
-  autoType : function(elementToTypeIn, text){
+  project1: document.getElementById("project1"),
+  project2: document.getElementById("project2"),
+  project3: document.getElementById("project3"),
+
+  title: document.getElementById("title"),
+  generatedText: document.getElementById("aboutMeText2"),
+  autoTypeIndex: 0,
+
+  showDescription: function () {
+      this.title.style.display = "none";
+      document.getElementById("subTitle").style.display = "none";
+      this.generatedText.style.display = "inline";
+  },
+
+  hideDescription: function () {
+      this.title.style.display = "block";
+      document.getElementById("subTitle").style.display = "block";
+      this.generatedText.innerHTML = "";
+      this.generatedText.style.display = "none";
+  },
+
+  createMouseEnterEventListener: function () {
+      document.querySelectorAll(".thumbnailContainer").forEach(function (item) {
+          item.addEventListener("mouseenter", function () {
+              if (autoTyper.generatedText.style.display == "none") {
+                  autoTyper.showDescription();
+                  autoTyper.setAutoType(item.id);
+              }
+          });
+      });
+  },
+
+  createMouseLeaveEventListener: function () {
+      document.querySelectorAll(".thumbnailContainer").forEach(function (item) {
+          item.addEventListener("mouseleave", function () {
+              if (autoTyper.generatedText.style.display == "inline") {
+                  autoTyper.hideDescription();
+                  autoTyper.cancelAutoType();
+              }
+          });
+      });
+  },
+
+  autoType: "",
+
+  setAutoType: function (description) {
+    let descriptionToWrite ;
+    switch (description) {
+      case "project1":
+        descriptionToWrite = this.description1;
+        break;
+
+      case "project2":
+        descriptionToWrite = this.description2;
+        break;
+
+      case "project3":
+        descriptionToWrite = this.description3;
+      break;
     
-    let x = 0;
-    let interval = setInterval(function(){
-      if(document.getElementById("title").style.display == "none"){
-        elementToTypeIn.innerHTML += text.charAt(x);
-        x++
-      } else{
-        clearInterval(interval);
-        return;
-      }
+      default:
+        console.log("error, did not find any corresponding description");
+        break;
+    }
 
-    }, 20);
-    
+    autoTyper.autoType = setInterval(function () {
+      autoTyper.generatedText.innerHTML += descriptionToWrite.charAt(autoTyper.autoTypeIndex);
+      autoTyper.autoTypeIndex++;
+  }, 10);
+  },
 
-  }
-}
+  cancelAutoType: function () {
+      autoTyper.autoTypeIndex = 0;
+      clearInterval(autoTyper.autoType);
+  },
+};
 
+console.log("1111" + autoTyper.autoType);
 
-
-var project1 = document.getElementById("project1");
-var project2 = document.getElementById("project2");
-var project3 = document.getElementById("project3");
-var title = document.getElementById("title")
-
-
-project1.addEventListener("mouseenter", function(){
-  if(title.style.display != "none"){
-    title.style.display = "none";
-    document.getElementById("subTitle").style.display = "none";
-    document.getElementById("aboutMeText2").style.display = "inline";
-    autoTyper.autoType(document.getElementById("aboutMeText2"),autoTyper.description1);
-  } 
-
-  
-});
-
-project1.addEventListener("mouseleave", function(){
-  if(title.style.display == "none"){
-    title.style.display = "block";
-    document.getElementById("subTitle").style.display = "block";
-    document.getElementById("aboutMeText2").innerHTML = "";
-    document.getElementById("aboutMeText2").style.display = "none";
-    
-  }
-});
-
-
-
-
-
-
+autoTyper.createMouseEnterEventListener();
+autoTyper.createMouseLeaveEventListener();
 
 // function shit() {
 //   if (i < text.length) {
